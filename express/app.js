9b597db598371438d35135a9b0a50b97d2cf5596
itsrahulbulchandani <73634522+itsrahulbulchandani@users.noreply.github.com>
2024-02-17 02:26:45 +0530

    http server checking

--- express/app.js
+++ express/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("user hit the server");
+  res.end("<h1>Found the page</h1>");
+});
+
+server.listen(5000);
