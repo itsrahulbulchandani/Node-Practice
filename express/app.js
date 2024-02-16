41310915be09e1b040499ff72380caf65f3131bc
itsrahulbulchandani <73634522+itsrahulbulchandani@users.noreply.github.com>
2024-02-17 02:42:23 +0530

    different urls

--- express/app.js
+++ express/app.js
@@ -1,8 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.end("<h1>Found the page</h1>");
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>Found the home page</h1>");
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>about page</h1>");
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("<h1>Wrong URL navigate to the home page</h1>");
+  }
 });
 
 server.listen(5000);
