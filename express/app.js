1352504b15f517b54d1b494fed9b15ad6cf58315
itsrahulbulchandani <73634522+itsrahulbulchandani@users.noreply.github.com>
2024-02-17 03:02:06 +0530

    sending other resources (css, logo, js) files as response using http server

--- express/app.js
+++ express/app.js
@@ -1,13 +1,27 @@
 const http = require("http");
+const { readFileSync } = require("fs");
 
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Found the home page</h1>");
+    res.end(homePage);
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.end("<h1>about page</h1>");
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.end(homeStyles);
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.end(homeImage);
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.end(homeLogic);
   } else {
     res.writeHead(404, { "content-type": "text/html" });
     res.end("<h1>Wrong URL navigate to the home page</h1>");
