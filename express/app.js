e50a474e7a75b3321cfde8bd940725e9004c2f19
itsrahulbulchandani <73634522+itsrahulbulchandani@users.noreply.github.com>
2024-02-17 03:09:33 +0530

    different urls with express

--- express/app.js
+++ express/app.js
@@ -1,31 +1,17 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end(homePage);
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>about page</h1>");
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.end(homeStyles);
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.end(homeImage);
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.end(homeLogic);
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.end("<h1>Wrong URL navigate to the home page</h1>");
-  }
+app.get("/", (req, res) => {
+  res.send("Home page");
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+app.listen(5000, () => {
+  console.log("server is listening on port 5000");
+});
