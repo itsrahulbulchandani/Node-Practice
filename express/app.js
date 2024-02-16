71898821b75479007879666e62bd3a68d86b8932
itsrahulbulchandani <73634522+itsrahulbulchandani@users.noreply.github.com>
2024-02-17 02:32:55 +0530

    simple header sending

--- express/app.js
+++ express/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
+  res.writeHead(200, { "content-type": "text/html" });
   res.end("<h1>Found the page</h1>");
 });
 
